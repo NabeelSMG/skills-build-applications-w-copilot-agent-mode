546d79b40bb642fd5213be3eb1fc2ff6eca93155
NabeelSMG <nabeel@silvermoongroup.com>
2025-07-30 13:59:48 +0000

    Fix Python path in launch.json, update allowed hosts and API URLs for Codespace

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-s5$)fz&x*+piq@2$#evlzslzj#z*ix+ak(yrxk@m6b#vl*w%lg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'friendly-dollop-pj649474jqrvh94wq-8000.app.github.dev']
 
 
 # Application definition
